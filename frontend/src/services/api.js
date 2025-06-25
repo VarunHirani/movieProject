c296bf2f74a00025dbe60ace93d8c03a9a4c51ac
VarunHirani <hirani.varun@gmail.com>
2025-06-25 18:57:06 +0530

    Initial commit with frontend React app.

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "a33d73c5f29303114d02a557436ef556";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
